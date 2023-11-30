868dd17d33b5958dbec30e8cfc874d2763734df2
Riquart Antonin <Antonin_riquart@ens.univ-artois.fr>
2023-11-30 11:11:10 +0100

    start project

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+
+import type AppNavbarVue from '~/components/nav/AppNavbar.vue';
+<template>
+  <div class="d-flex flex-column default-layout">
+      <NavAppNavbar/>
+
+      <main class="default-layout-content overflow-hidden">
+          <slot />
+      </main>
+
+      <NavAppFooter/>
+  </div>
+</template>
+
+<style>
+.default-layout{
+  display: flex ;
+  flex-direction: column;
+}
+.default-layout-content{
+    flex-grow:1;
+}
+.default-layout{
+    min-height: 100vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>test nuxt app</h1>
+  </div>
+</template>
