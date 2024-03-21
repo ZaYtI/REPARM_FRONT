e2b891ba86a522be05e1f064737bef54c19508ac
ZaYtI <antonin_riquart@ens.univ-artois.fr>
2024-03-21 23:46:21 +0100

    📱 Add responsiv design for select Categorie

--- components/product/categorieSelect.vue
+++ components/product/categorieSelect.vue
@@ -9,21 +9,48 @@ onMounted(async ()=>{
 })
 </script>
 <template>
-  <div class="container-fluid  px-0 pt-3 selected-cat-container">
-    <div class="d-flex justify-content-center button-wrapper">
+  <div class="px-0 pt-3 selected-cat-container">
+    <div class="justify-content-center button-wrapper d-lg-flex d-none">
       <div class="button-container">
         <button
-          class="btn mx-3 text-uppercase"
+          class="btn btn-start mx-3 text-uppercase"
           @click="() => store.setSelectedCat(0,'Tous les fusils')"
           :class="{ 'active': store.getSelectedCat === 'Tous les fusils' }"
         >Tous les fusils</button>
       </div>
       <div class="button-container" v-for="cat in store.getListOfCategorie" :key="cat">
         <button
-          class="btn mx-3 text-uppercase"
+          class="btn btn-start mx-3 text-uppercase"
           @click="() => store.setSelectedCat(cat.id,cat.name)"
           :class="{ 'active': store.getSelectedCat === cat.name }"
         >{{ cat.name }}</button>
+      </div>
+    </div>
+    <div class="d-lg-none justify-content-center px-5">
+      <div class="d-flex justify-content-center">
+        <button class="btn btn-lg btn-collapse dropdown-toggle d-flex align-items-center justify-content-center w-100" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+          Selectionner une catégorie
+        </button>
+      </div>
+      <div class="collapse rounded-bottom" id="collapseExample">
+        <div class="card card-body">
+          <div class="justify-content-center">
+            <div class="button-container">
+              <button
+                class="btn btn-phone w-100 text-uppercase"
+                @click="() => store.setSelectedCat(0,'Tous les fusils')"
+                :class="{ 'active': store.getSelectedCat === 'Tous les fusils' }"
+              >Tous les fusils</button>
+            </div>
+            <div class="button-container" v-for="cat in store.getListOfCategorie" :key="cat">
+              <button
+                class="btn btn-phone w-100 text-uppercase"
+                @click="() => store.setSelectedCat(cat.id,cat.name)"
+                :class="{ 'active': store.getSelectedCat === cat.name }"
+              >{{ cat.name }}</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <div>
@@ -33,7 +60,40 @@ onMounted(async ()=>{
 </template>
 
 <style lang="scss" scoped>
-.btn {
+
+.btn-phone:not(.active){
+  background-color: white;
+  color: #B54A29;
+}
+
+.btn-phone{
+  background-color: #B54A29;
+  color: white;
+}
+
+.btn-collapse{
+  background-color: white;
+  color: #B54A29;
+  transition: all .2s ease-in-out;  
+}
+
+.btn-collapse:not(.collapsed){
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+  border-bottom-color:white ;
+  box-shadow: none;
+}
+
+.collapsed{
+
+}
+
+.card{
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+  border: none;
+}
+.btn-start {
   position: relative;
   color: white;
   outline: none;
